ee08b2a4ffafbb903c11be8e7f376c3a4f52f24a
Arif <arif.onedirection0011@gmail.com>
2024-08-28 14:30:35 +0600

    added string join

--- basics/02_printing_and_formatting_strings.go
+++ basics/02_printing_and_formatting_strings.go
@@ -17,28 +17,25 @@ func main() {
 	fmt.Print(age)
 	fmt.Print(" years old.")
 	fmt.Print("\n\n")
-	
+
 	// Print, with new line
 	fmt.Println("Hello!, ")
-	fmt.Println("I am", name, ".")		// comma separated adds a space, mind it, sweetie!
+	fmt.Println("I am", name, ".") // comma separated adds a space, mind it, sweetie!
 	fmt.Println("and I am", age, "years old.")
 
 	// Printf  (formatted string) %_ = format specifier
-	fmt.Printf("Hello!, I am %v. and I am %v years old.\n", name, age)				// %v = value, default for variables
-	fmt.Printf("Hello!, I am %s. and I am %d years old.\n", name, age)				// %s = string, %d = int
-	fmt.Printf("Hello!, I am %q. and I am %q years old.\n", name, age)				// %q = quoted string, doesn't work with numbers. Useful for strings with spaces.
-	fmt.Printf("name is a type of %T. and age is a type of %T .\n", name, age)		// %T = type
-	fmt.Printf("You scored %f points.\n", 10.5652)									// %f = float
-	fmt.Printf("You scored %F points.\n", 10.5652)									// %F = synonym for %f
-	fmt.Printf("You scored %0.2f points.\n", 10.5652)								// %0.2f = float with 2 decimals
+	fmt.Printf("Hello!, I am %v. and I am %v years old.\n", name, age)         // %v = value, default for variables
+	fmt.Printf("Hello!, I am %s. and I am %d years old.\n", name, age)         // %s = string, %d = int
+	fmt.Printf("Hello!, I am %q. and I am %q years old.\n", name, age)         // %q = quoted string, doesn't work with numbers. Useful for strings with spaces.
+	fmt.Printf("name is a type of %T. and age is a type of %T .\n", name, age) // %T = type
+	fmt.Printf("You scored %f points.\n", 10.5652)                             // %f = float
+	fmt.Printf("You scored %F points.\n", 10.5652)                             // %F = synonym for %f
+	fmt.Printf("You scored %0.2f points.\n", 10.5652)                          // %0.2f = float with 2 decimals
 
-	fmt.Printf("Hello!, I am %p. and I am %p years old.\n", name, age)				// %p = address of 0th element in base 16 notation, with leading 0x
-
-	
+	fmt.Printf("Hello!, I am %p. and I am %p years old.\n", name, age) // %p = address of 0th element in base 16 notation, with leading 0x
 
 	// Sprintf (save formatted string)
 	s := fmt.Sprintf("Hello!, I am %s. and I am %d years old.", name, age)
 	fmt.Println(s)
-
 
 }
